6f1a1405a6aa624dcb94525618aeb8b71d6155df
febriaricandra <febriaricandraproduction@gmail.com>
2022-06-26 15:25:58 +0700

    init boilerplate

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link underline decoration-sky-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
